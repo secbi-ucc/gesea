f653c2bc8834438b0864762106f7fb02cf72ba99
Julio César <julioc255io@gmail.com>
2016-09-09 17:03:16 -0500

    urls actvidades

--- control_bienestar/urls.py
+++ control_bienestar/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^actividades/$', 'actividades.views.lista_actividades', name='actividades-lista'),
+    url(r'^actividades/detalle/(?P<id_actividad>\d+)', 'actividades.views.detalle_actividad', name='actividades-detalle'),
 )
