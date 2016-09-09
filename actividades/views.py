6207066fa34688ce61dc4aa3534e965b6ea130f1
Julio César <julioc255io@gmail.com>
2016-09-09 17:03:04 -0500

    Agregar views detalle/ lista actividades

--- actividades/views.py
+++ actividades/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Actividad
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def lista_actividades(request):
+
+    a = Actividad.objects.all()
+
+    return render(request, 'actividades/lista_actividades.html', {'a':a})
+
+
+def detalle_actividad(request, id_actividad):
+
+    b = get_object_or_404(Actividad, pk=id_actividad)
+
+    return render(request, 'actividades/detalle_actividad.html', {'b':b})
