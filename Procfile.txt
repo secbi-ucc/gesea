55bbe2aa92d2907c5f6ca8d1a7ddad701155e52c
Reinaldo López Sobrino <reyloso2@gmail.com>
2017-03-16 21:24:58 -0500

    server config

---
+++ Procfile.txt
@@ -0,0 +1,1 @@
+web: gunicorn control_bienestar.wsgi --log-file -
--- control_bienestar/settings.py
+++ control_bienestar/settings.py
@@ -90,10 +90,13 @@ WSGI_APPLICATION = 'control_bienestar.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': { 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'postgres',
+      'HOST': 'localhost',
+      'PORT': '5432',
+      'USER': 'postgres',
+      'PASSWORD': '12345'
+  }
 }
 
 # Internationalization
