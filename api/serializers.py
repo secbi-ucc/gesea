7c750bafef4e664b2b0d341aff68acc5037a48af
Julio César <julioc255io@gmail.com>
2017-05-02 08:44:08 -0500

    misc

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from rest_framework import serializers
 from inscripcion.models import AsistenciaEstudiante, Estudiantes, Instructor
 from programacion.models import Programacion, DiaActividad, Horario
+from rest_framework import serializers
 
 
 class HorarioSerializer(serializers.HyperlinkedModelSerializer):
