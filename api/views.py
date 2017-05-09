dc0666d65208b1525d0d5a64dc5502295323235f
Julio César <julioc255io@gmail.com>
2017-05-08 20:27:12 -0500

    remove custom list response

--- api/views.py
+++ api/views.py
@@ -102,14 +102,6 @@ class AsistenciaEstudianteViewSet(viewsets.ModelViewSet):
     queryset = AsistenciaEstudiante.objects.all()
     paginate_by = None
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        serializer_data = serializer.data
-        custom_data = {'asistencia_results': {'asistencia_array': serializer_data}}
-        return Response(custom_data)
-
-
     def get_queryset(self):
         user = self.request.user
         p = Programacion.objects.filter(Instructor__user=user).last()
